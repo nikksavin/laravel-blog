b43d9378b9e97a8e2f5987e36c61bb0503ae563a
Nicholas <mykolakratasyk@gmail.com>
2023-04-05 14:17:59 +0300

    start

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+
+class PostController extends Controller
+{
+    //
+    public function index() {
+        $post = Post::find(1);
+        dd($post);
+    }
+
+}
